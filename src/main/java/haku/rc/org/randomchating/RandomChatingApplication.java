5253c546c1c321aa3b2824890dc1264c004a6eac
songzio <zioziozzang@naver.com>
2026-04-14 22:24:30 +0900

    first commit

---
+++ src/main/java/haku/rc/org/randomchating/RandomChatingApplication.java
@@ -0,0 +1,13 @@
+package haku.rc.org.randomchating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomChatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomChatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/haku/rc/org/randomchating/RandomChatingApplicationTests.java
@@ -0,0 +1,13 @@
+package haku.rc.org.randomchating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomChatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
